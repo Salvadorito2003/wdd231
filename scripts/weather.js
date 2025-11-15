8f4a6f3c914527a0f811a0e8b050d81a13492b55
Tobias <tobiasaguilartarea@gmail.com>
2025-11-15 18:02:43 -0300

    Practica

---
+++ scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const myKey = "552b4e30cfa351081e49909aba5677fc";
+const myLat = "49.747159953465044";
+const myLon = "6.641206584733308";
+
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}`;
+
+
+async function apiFetch() {
+    try {
+        const response = await(url);
+        if(response.ok) {
+            const data = await response.json();
+            crossOriginIsolated.log(data);
+        } else {
+            throw error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    console.log("hello");
+}
+
+displayResults();
