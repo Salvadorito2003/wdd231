70a96e0857b1acf59e02e71469c98b202b3e9934
Tobias <tobiasaguilartarea@gmail.com>
2025-12-02 22:00:24 -0300

    Project site plan

---
+++ finalproject/scripts/date.js
@@ -0,0 +1,7 @@
+const yearElement = document.querySelector("#currentyear");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = `© ${currentYear} Argentina Mission Store`;
+
+const lastModifiedElement = document.querySelector("#lastmodified");
+lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
+
